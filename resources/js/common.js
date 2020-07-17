6ba6e036254cbd7f3a705e5a09aadcaf96ac9e9a
Anatoly Zhuravlev <37895952+newstreetpunk@users.noreply.github.com>
2020-07-17 18:22:30 +0500

    Подключение библиотек, верстка шапки

--- resources/js/common.js
+++ resources/js/common.js
@@ -1,8 +1,28 @@
 jQuery(function($) {
 	
-	$('.mobile-btn').on('click', function() {
-		$(this).toggleClass('active');
-		$('.header__menu').toggleClass('open');
+	var h_hght = 30;
+	var h_mrg = 0;
+	var elem = $('#header');
+	var top = $(this).scrollTop();
+
+	if(top > h_hght){
+		elem.css('top', h_mrg);
+	}           
+
+	$(window).scroll(function(){
+		top = $(this).scrollTop();
+
+		if (top+h_mrg < h_hght) {
+			elem.css({
+				'top': h_hght-top,
+				'background-color': 'transparent'
+			});
+		} else {
+			elem.css({
+				'top': h_mrg,
+				'background-color': '#000'
+			});
+		}
 	});
 
 });
