611956853c00916860d0a813c7253a30db64286e
Anatoly Zhuravlev <37895952+newstreetpunk@users.noreply.github.com>
2020-08-25 15:43:16 +0500

    adaptive reviews

--- resources/js/common.js
+++ resources/js/common.js
@@ -84,8 +84,23 @@ jQuery(function($) {
 		infinite: true,
 		slidesToShow: 3,
 		slidesToScroll: 1,
-		dots: false,
+		dots: true,
 		arrows: false,
+		adaptiveHeight: true,
+		responsive: [
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 2,
+			}
+		},
+		{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 1,
+			}
+		},
+		]
 	});
 
 
