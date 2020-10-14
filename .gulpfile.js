149556590f9471c5d530cb3c25a6da4a63d7e858
Alexander Sabanov <alexsab@mail.ru>
2020-10-14 12:38:00 +0400

    upd gulpfile and packages

--- .gulpfile.js
+++ .gulpfile.js
@@ -1,56 +1,105 @@
-// npm install gulp-pngquant --save-dev
-// npm uninstall gulp-pngquant
-// npm install imagemin-pngquant --save-dev
+// VARIABLES & PATHS
+let preprocessor = 'sass', // Preprocessor (sass, scss, less, styl)
+    fileswatch   = 'html,htm,txt,json,md,woff2,php', // List of files extensions for watching & hard reload (comma separated)
+    pageversion  = 'html,htm,php', // List of files extensions for watching change version files (comma separated)
+    imageswatch  = 'jpg,jpeg,png,webp,svg', // List of images extensions for watching & compression (comma separated)
+    online       = true, // If «false» - Browsersync will work offline without internet connection
+    basename     = require('path').basename(__dirname),
+    forProd      = [
+					'/**',
+					' * @author Alexsab.ru',
+					' */',
+					''].join('\n');
 
-pngquant = require('imagemin-pngquant')
+const { src, dest, parallel, series, watch, task } = require('gulp'),
+	sass           = require('gulp-sass'),
+	cleancss       = require('gulp-clean-css'),
+	concat         = require('gulp-concat'),
+	browserSync    = require('browser-sync').create(),
+	uglify         = require('gulp-uglify-es').default,
+	autoprefixer   = require('gulp-autoprefixer'),
+	imagemin       = require('gulp-imagemin'),
+	newer          = require('gulp-newer'),
+	rsync          = require('gulp-rsync'),
+	del            = require('del'),
+	connect        = require('gulp-connect-php'),
+	header         = require('gulp-header'),
+	notify         = require('gulp-notify'),
+	rename         = require('gulp-rename'),
+	responsive     = require('gulp-responsive'),
+	pngquant       = require('imagemin-pngquant'),
+	merge          = require('merge-stream'),
+	// version        = require('gulp-version-number'),
+	// revAll         = require('gulp-rev-all'),
+	replace        = require('gulp-replace');
 
-	alfa_center: {
-		port: ++port,
+if(typeof projects == 'undefined') 
+	global.projects = {};
+if(typeof port == 'undefined') 
+	global.port = 8100;
 
-		base: base.alfa_center,
-		dest: base.alfa_center,
+function makeid() {
+  return (Math.random() + 1).toString(36).substring(7);
+}
 
-		styles: {
-			src:    base.alfa_center + '/resources/' + preprocessor + '/**/*',
-			//watch:  base.alfa_center + '/resources/' + preprocessor + '/**/*.'+preprocessor,
-			dest:   base.alfa_center + '/public/css',
-			output: 'main.min.css',
-		},
 
-		scripts: {
-			src: [			
-				base.alfa_center + '/resources/libs/jquery/jquery-2.2.4.min.js',
-				base.alfa_center + '/resources/libs/Magnific-Popup-master/jquery.magnific-popup.js',
-				'node_modules/slick-carousel/slick/slick.min.js',
-				base.alfa_center + '/resources/libs/lazyload/lazyload.js',
-				base.alfa_center + '/resources/libs/waypoints.min.js',
-				base.alfa_center + '/resources/libs/animate/animate-css.js',
-				base.alfa_center + '/resources/libs/sweetalert.min.js',
-				base.alfa_center + '/resources/js/common.js',
-			],
-			dest:       base.alfa_center + '/public/js',
-			output:     'scripts.min.js',
-		},
+projects.alfa_center = {
+	
+	port: ++port,
 
-		images: {
-			src:  base.alfa_center + '/resources/img/**/*',
-			dest: base.alfa_center + '/public/img',
-		},
+	base: basename,
+	dest: basename,
 
-		code: {
-			src: [
-				base.alfa_center  + '/**/*.{' + fileswatch + '}'
-			],
-		},
-		forProd: [
-			'/**',
-			' * @author https://github.com/newstreetpunk',
-			' * @editor https://github.com/alexsab',
-			' */',
-			''].join('\n'),
+	styles: {
+		src:    basename + '/resources/' + preprocessor + '/**/*',
+		//watch:  basename + '/resources/' + preprocessor + '/**/*.'+preprocessor,
+		dest:   basename + '/public/css',
+		output: 'main.min.css',
 	},
 
+	scripts: {
+		src: [
+			basename + '/resources/libs/jquery/jquery-2.2.4.min.js',
+			basename + '/resources/libs/Magnific-Popup-master/jquery.magnific-popup.js',
+			'node_modules/slick-carousel/slick/slick.js',
+			basename + '/resources/libs/lazyload/lazyload.js',
+			basename + '/resources/libs/waypoints.min.js',
+			basename + '/resources/libs/animate/animate-css.js',
+			basename + '/resources/libs/sweetalert.min.js',
+			basename + '/resources/js/common.js',
+		],
+		dest:       basename + '/public/js',
+		output:     'scripts.min.js',
+	},
+
+	images: {
+		src:  basename + '/resources/img/**/*',
+		dest: basename + '/public/img',
+	},
+
+	code: {
+		src: [
+			basename  + '/**/*.{' + fileswatch + '}',
+		],
+	},
+	version: {
+		src: [
+			basename  + '/**/*.{' + pageversion + '}',
+		],
+	},
+	forProd: [
+		'/**',
+		' * @author https://github.com/newstreetpunk',
+		' * @editor https://github.com/alexsab',
+		' */',
+		''].join('\n'),
+}
+
+
+
+
 /* alfa_center BEGIN */
+
 // Local Server
 function alfa_center_browsersync() {
 	connect.server({
@@ -75,8 +124,68 @@ function alfa_center_styles() {
 	.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Optional. Comment out when debugging
 	.pipe(dest(projects.alfa_center.styles.dest))
 	.pipe(browserSync.stream())
-
 };
+
+// function alfa_center_manifest() {
+// 	return src([projects.alfa_center.styles.src])
+// 	.pipe(eval(preprocessor)({ outputStyle: 'expanded' }).on("error", notify.onError()))
+// 	.pipe(concat(projects.alfa_center.styles.output))
+// 	.pipe(autoprefixer({ grid: true, overrideBrowserslist: ['last 10 versions'] }))
+// 	.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Optional. Comment out when debugging
+// 	.pipe(dest(projects.alfa_center.styles.dest))
+// 	.pipe(revAll.revision())
+// 	.pipe(dest(projects.alfa_center.styles.dest))
+// 	.pipe(revAll.versionFile())
+// 	.pipe(dest(projects.alfa_center.styles.dest));
+// };
+// exports.alfa_center_manifest = alfa_center_manifest;
+
+// function alfa_center_version() {
+// 	return src(projects.alfa_center.code.src)
+//     .pipe(version({
+// 			'value': '%MDS%',
+// 			'append': {
+// 				'key': 'v',
+// 				'to': ['css', 'js'],
+// 			},
+// 		})
+//     )
+//     .pipe(dest(function (file) {
+//         return file.base;
+//     }));
+// };
+// exports.alfa_center_version = alfa_center_version;
+
+exports.alfa_center_versioningCss = () => {
+  return src(projects.alfa_center.code.src)
+    .pipe(replace(/(.*)\.css\?(v=.+&)*(.*)/g, '$1.css?v='+makeid()+'&$3'))
+    .pipe(replace(/(.*)\.css\"(.*)/g, '$1.css?v='+makeid()+'"$2'))
+    .pipe(replace(/(.*)\.css\'(.*)/g, '$1.css?v='+makeid()+'\'$2'))
+    .pipe(dest(function (file) {
+        return file.base;
+    }));
+};
+
+exports.alfa_center_versioningJs = () => {
+  return src(projects.alfa_center.code.src)
+    .pipe(replace(/(.*)\.js\?(v=.+&)*(.*)/g, '$1.js?v='+makeid()+'&$3'))
+    .pipe(replace(/(.*)\.js\"(.*)/g, '$1.js?v='+makeid()+'"$2'))
+    .pipe(replace(/(.*)\.js\'(.*)/g, '$1.js?v='+makeid()+'\'$2'))
+    .pipe(dest(function (file) {
+        return file.base;
+    }));
+};
+
+exports.alfa_center_versioningImage = () => {
+  return src(projects.alfa_center.code.src)
+    .pipe(replace(/(.*)\.(png|jpg|jpeg|gif)\?(_v=.+&)*(.*)/g, '$1.$2?v='+makeid()+'&$4'))
+    .pipe(replace(/(.*)\.(png|jpg|jpeg|gif)\"(.*)/g, '$1.$2?v='+makeid()+'"$3'))
+    .pipe(replace(/(.*)\.(png|jpg|jpeg|gif)\'(.*)/g, '$1.$2?v='+makeid()+'\'$3'))
+    .pipe(dest(function (file) {
+        return file.base;
+    }));
+};
+
 
 // Scripts
 function alfa_center_scripts() {
@@ -111,6 +220,7 @@ function alfa_center_watch() {
 	watch(projects.alfa_center.code.src).on('change', browserSync.reload);
 };
 
-exports.alfa_center = parallel(alfa_center_images, alfa_center_styles, alfa_center_scripts, alfa_center_browsersync, alfa_center_watch);
+module.exports = parallel(alfa_center_images, alfa_center_styles, alfa_center_scripts, alfa_center_browsersync, alfa_center_watch);
 
 /* alfa_center END */
+
