e958ceb3a49006e89d7a2bde67f2f03138ed8244
Anatoly Zhuravlev <37895952+newstreetpunk@users.noreply.github.com>
2021-03-24 11:56:17 +0500

    Added logo
    added logo, styles
    edited scripts

--- resources/js/common.js
+++ resources/js/common.js
@@ -51,15 +51,15 @@ jQuery(function($) {
 		return false;
 	});
 
-	$('h1').animated('fadeInDown', 'fadeInDown');
+	$('h1, .header').animated('fadeInDown', 'fadeInDown');
 
 	$('.banner__subtitle, section h2, .services-block, .callback-section, section .sect-subtitle, .btn-wrap, .brands')
 	.animated('fadeInUp', 'fadeInUp');
 
-	$('.header__phone, .banner__discount-list, .advantages-item:nth-child(odd)')
+	$('.banner__discount-list, .advantages-item:nth-child(odd)')
 	.animated('fadeInLeft', 'fadeInLeft');
 
-	$('.header__address, .banner__form, .advantages-item:nth-child(even)')
+	$('.banner__form, .advantages-item:nth-child(even)')
 	.animated('fadeInRight', 'fadeInRight');
 
 
@@ -91,7 +91,7 @@ jQuery(function($) {
 
 	function customResize(){
 		if ( $(window).width() > 992 ) {
-			h_hght = 30;
+			h_hght = 1;
 			$('header').css({
 				'top': h_hght,
 				'background-color': 'transparent'
